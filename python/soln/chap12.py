af0c25de3c0428c294bf2c3eb384edadfad6ecc7
Allen Downey <downey@allendowney.com>
2021-02-05 15:56:17 -0500

    Revisions

--- python/soln/chap12.py
+++ python/soln/chap12.py
@@ -33,3 +33,10 @@ def calc_total_infected(results, system):
     s_end = results.S[system.t_end]
     return s_0 - s_end
 
+from modsim import *
+
+def calc_total_infected(results, system):
+    s_0 = results.S[system.t0]
+    s_end = results.S[system.t_end]
+    return s_0 - s_end
+
